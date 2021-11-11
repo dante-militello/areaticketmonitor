1bbb62a9638802765a8011d65fad9ce4dc75633f
Franco Romaniello <romax.cs@gmail.com>
2021-11-11 06:01:40 +0100

    FIX Cannot read property 'body' of undefined

--- index.js
+++ index.js
@@ -14,50 +14,54 @@ function check() {
 
   request(options, function (error, response) {
     if (error) throw new Error(error);
-
-    if (response.body) {
-      try {
-        estado = JSON.parse(response.body).performances[0].statusName;
-      } catch (e) {
-        console.log("No se pudo parsear, error en el sitio intento nuevamente...");
-        if (e.code === 'ETIMEDOUT') {
-          console.log('Timeout, pero reintentamos!');
-        }
-      }
-
-      if (typeof estado !== 'undefined') {
-
-        if (estado == "Disponible Pronto") {
-
-          console.log("Disponible Pronto, reintentando...");
-
-        } else {
-          console.log("TICKETS DISPONIBLES!!!");
-          exec('start /B "node" "alarma.mp3"');
-
-          if (process.env.TGRAM == 1) {
-            var requestOptions = {
-              method: 'GET',
-              redirect: 'follow'
-            };
-
-            var options = {
-              'method': 'GET',
-              'url': 'https://api.telegram.org/bot' + process.env.TOKENBOT + '/sendMessage?chat_id=' + process.env.TGRAMID + '&text=' + process.env.MENSAJE,
-              'headers': {
-              }
-            };
-            request(options, function (error, response) {
-              if (error) throw new Error(error);
-              console.log(response.body);
-            });
-
-            esperar = process.env.ESPERARBETWEEN;
+    try {
+      if (response.body) {
+        try {
+          estado = JSON.parse(response.body).performances[0].statusName;
+        } catch (e) {
+          console.log("No se pudo parsear, error en el sitio intento nuevamente...");
+          if (e.code === 'ETIMEDOUT') {
+            console.log('Timeout, pero reintentamos!');
           }
         }
 
+        if (typeof estado !== 'undefined') {
+
+          if (estado == "Disponible Pronto") {
+
+            console.log("Disponible Pronto, reintentando...");
+
+          } else {
+            console.log("TICKETS DISPONIBLES!!!");
+            exec('start /B "node" "alarma.mp3"');
+
+            if (process.env.TGRAM == 1) {
+              var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+              };
+
+              var options = {
+                'method': 'GET',
+                'url': 'https://api.telegram.org/bot' + process.env.TOKENBOT + '/sendMessage?chat_id=' + process.env.TGRAMID + '&text=' + process.env.MENSAJE,
+                'headers': {
+                }
+              };
+              request(options, function (error, response) {
+                if (error) throw new Error(error);
+                console.log(response.body);
+              });
+
+              esperar = process.env.ESPERARBETWEEN;
+            }
+          }
+
+        }
+        setTimeout(check, esperar);
       }
-      setTimeout(check, esperar);
+    }
+    catch(e) {
+         console.log("No se pudo parsear, error en el sitio intento nuevamente...");
     }
   });
 }
