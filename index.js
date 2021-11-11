de8dc2c27f0b7159cf378bbe744d0aca0726ef12
Franco Romaniello <romax.cs@gmail.com>
2021-11-11 02:07:16 -0300

    ADD setTimeout to the catch block

--- index.js
+++ index.js
@@ -3,66 +3,69 @@ require('dotenv').config();
 var exec = require('child_process').exec;
 
 function check() {
+	esperar = process.env.CHECKMS;
+	eventoid = process.env.EVENTOID;
 
-  esperar = process.env.CHECKMS;
-  eventoid = process.env.EVENTOID;
+	var options = {
+		'method': 'GET',
+		'url': 'https://ventas.areaticket.com.ar/api2/v1/events/' + eventoid
+	};
 
-  var options = {
-    'method': 'GET',
-    'url': 'https://ventas.areaticket.com.ar/api2/v1/events/' + eventoid
-  };
+	request(options, function (error, response) {
+		if (error) {
+			throw new Error(error);
+		}
+		try {
+			if (response.body) {
+				try {
+					estado = JSON.parse(response.body).performances[0].statusName;
+				}
+				catch (e) {
+					console.log("No se pudo parsear, error en el sitio intento nuevamente...");
+					
+					if (e.code === 'ETIMEDOUT') {
+						console.log('Timeout, pero reintentamos!');
+					}
+				}
 
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    try {
-      if (response.body) {
-        try {
-          estado = JSON.parse(response.body).performances[0].statusName;
-        } catch (e) {
-          console.log("No se pudo parsear, error en el sitio intento nuevamente...");
-          if (e.code === 'ETIMEDOUT') {
-            console.log('Timeout, pero reintentamos!');
-          }
-        }
+				if (typeof estado !== 'undefined') {
+					if (estado == "Disponible Pronto") {
+						console.log("Disponible Pronto, reintentando...");
+					}
+					else {
+						console.log("TICKETS DISPONIBLES!!!");
+						exec('start /B "node" "alarma.mp3"');
 
-        if (typeof estado !== 'undefined') {
+						if (process.env.TGRAM == 1) {
+							var requestOptions = {
+								method: 'GET',
+								redirect: 'follow'
+							};
 
-          if (estado == "Disponible Pronto") {
+							var options = {
+								'method': 'GET',
+								'url': 'https://api.telegram.org/bot' + process.env.TOKENBOT + '/sendMessage?chat_id=' + process.env.TGRAMID + '&text=' + process.env.MENSAJE,
+								'headers': {
+								}
+							};
+							request(options, function (error, response) {
+								if (error) throw new Error(error);
+								console.log(response.body);
+							});
 
-            console.log("Disponible Pronto, reintentando...");
+							esperar = process.env.ESPERARBETWEEN;
+						}
+					}
 
-          } else {
-            console.log("TICKETS DISPONIBLES!!!");
-            exec('start /B "node" "alarma.mp3"');
-
-            if (process.env.TGRAM == 1) {
-              var requestOptions = {
-                method: 'GET',
-                redirect: 'follow'
-              };
-
-              var options = {
-                'method': 'GET',
-                'url': 'https://api.telegram.org/bot' + process.env.TOKENBOT + '/sendMessage?chat_id=' + process.env.TGRAMID + '&text=' + process.env.MENSAJE,
-                'headers': {
-                }
-              };
-              request(options, function (error, response) {
-                if (error) throw new Error(error);
-                console.log(response.body);
-              });
-
-              esperar = process.env.ESPERARBETWEEN;
-            }
-          }
-
-        }
-        setTimeout(check, esperar);
-      }
-    }
-    catch(e) {
-         console.log("No se pudo parsear, error en el sitio intento nuevamente...");
-    }
-  });
+				}
+				setTimeout(check, esperar);
+			}
+		}
+		catch(e) {
+			console.log("No se pudo parsear, error en el sitio intento nuevamente...");
+			setTimeout(check, esperar);
+		}
+	});
 }
+
 check();
